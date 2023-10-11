14f6537af4b7a653e4e7c88d04465b9c1cd62714
Rushikesh <rushichoudhari040@gmail.com>
2023-10-11 16:57:20 +0530

    new-2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,24 +5,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent implements OnInit {
- 
+
   title = 'practice1';
 
 
 
 
 
-   constructor(
- 
-   )  {}
- 
+  constructor() { }
+
 
 
   ngOnInit(): void {
-  
+
   }
 
 
